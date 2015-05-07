7a9e46b4950e469df9ecb3a6068b9ab42469de31
Tyler Almond <tyler@tamedia.ca>
2015-05-07 10:02:31 -0400

    Update contactcontroller.js

--- app/scripts/controllers/contactcontroller.js
+++ app/scripts/controllers/contactcontroller.js
@@ -39,7 +39,7 @@ angular.module('normliV2App')
             $http({
                 method  : 'POST',
                 url     : 'contact-form.php',
-                data    : $.param($scope.formData),  //param method from jQuery
+                data    : $.param(angular.copy($scope.formData)),  //param method from jQuery
                 headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
             }).success(function(data){
                 console.log(data);
